a1c32efab1322eb463d4950921b80e9968053732
Louis Lu <louis.lu@synerty.com>
2021-03-05 04:46:26 +0000

    PEEK-836 Used 'callMethodLater' decorator

--- peek_core_device/_private/server/controller/NotifierController.py
+++ peek_core_device/_private/server/controller/NotifierController.py
@@ -1,12 +1,13 @@
 import logging
 from datetime import datetime
 
+from vortex.DeferUtil import callMethodLater
+from vortex.TupleSelector import TupleSelector
+from vortex.handler.TupleDataObservableHandler import TupleDataObservableHandler
+
 from peek_core_device._private.storage.DeviceUpdateTuple import \
     DeviceUpdateTuple
 from peek_core_device.tuples.DeviceInfoTuple import DeviceInfoTuple
-from vortex.DeferUtil import callLaterWrap
-from vortex.TupleSelector import TupleSelector
-from vortex.handler.TupleDataObservableHandler import TupleDataObservableHandler
 
 logger = logging.getLogger(__name__)
 
@@ -26,17 +27,8 @@ class NotifierController:
         self._tupleObservable = None
         self._api = None
 
-    @callLaterWrap(seconds=0.0)
+    @callMethodLater
     def notifyDeviceInfo(self, deviceId: str):
-        self._notifyDeviceInfoObservable(deviceId)
-
-    def _notifyDeviceInfoObservable(self, deviceId: str):
-        """Notify the observer of the update
-
-        This tuple selector must exactly match what the UI observes
-
-        """
-
         self._tupleObservable.notifyOfTupleUpdate(
             TupleSelector(DeviceInfoTuple.tupleName(), dict(deviceId=deviceId))
         )
@@ -45,17 +37,8 @@ class NotifierController:
             TupleSelector(DeviceInfoTuple.tupleName(), dict())
         )
 
-    @callLaterWrap(seconds=0.0)
+    @callMethodLater
     def notifyDeviceUpdate(self, deviceType: str):
-        self._notifyDeviceUpdateObservable(deviceType)
-
-    def _notifyDeviceUpdateObservable(self, deviceType: str):
-        """Notify the observer of the update
-
-        This tuple selector must exactly match what the UI observes
-
-        """
-
         self._tupleObservable.notifyOfTupleUpdate(
             TupleSelector(DeviceUpdateTuple.tupleName(),
                 dict(deviceType=deviceType))
@@ -65,36 +48,17 @@ class NotifierController:
             TupleSelector(DeviceUpdateTuple.tupleName(), dict())
         )
 
-    @callLaterWrap(seconds=0.0)
+    @callMethodLater
     def notifyDeviceOnline(self, deviceId: str, deviceToken: str, online: bool):
         """Notify Device Online
 
         Notify that the device has changed it's online status
 
         """
-        self._notifyDeviceOnlineObservable(deviceId, deviceToken, online)
-
-    def _notifyDeviceOnlineObservable(
-        self, deviceId: str, deviceToken: str, online: bool
-    ):
         self._api.notifyOfOnlineStatus(deviceId, deviceToken, online)
 
-    @callLaterWrap(seconds=0.0)
+    @callMethodLater
     def notifyDeviceGpsLocation(
-        self,
-        deviceToken: str,
-        latitude: float,
-        longitude: float,
-        updatedDate: datetime,
-    ):
-        self._notifyDeviceGpsLocationObservable(
-            deviceToken,
-            latitude,
-            longitude,
-            updatedDate,
-        )
-
-    def _notifyDeviceGpsLocationObservable(
         self,
         deviceToken: str,
         latitude: float,
