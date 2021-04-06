e167fcf60b09954e5fa1660383c3bea723fb974c
Jacob Sorrell <jacob.sorrell@synerty.com>
2021-04-06 13:48:39 +1000

    PEEK-1073-1 Update types and syntax

--- peek_core_device/plugin-module/_private/gps/private-gps-location.service.ts
+++ peek_core_device/plugin-module/_private/gps/private-gps-location.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core"
-import { BehaviorSubject, combineLatest } from "rxjs"
+import { BehaviorSubject, combineLatest, Observable } from "rxjs"
 
 import {
     DeviceGpsLocationService,
@@ -17,10 +17,10 @@ const {Geolocation} = Plugins
 @Injectable()
 export class PrivateDeviceGpsLocationService extends DeviceGpsLocationService {
     private _location$ = new BehaviorSubject<DeviceGpsLocationTuple | null>(null)
-    private gpsWatchId: string
+    private gpsWatchId: string | null
     private lastSeenPositionTupleAction: GpsLocationUpdateTupleAction
     private deviceId: string
-    
+
     constructor(
         private tupleService: DeviceTupleService,
         private deviceService: DeviceEnrolmentService,
@@ -32,56 +32,67 @@ export class PrivateDeviceGpsLocationService extends DeviceGpsLocationService {
             this.userService.loggedInStatus,
             this.deviceService.deviceInfoObservable()
         )
-            .subscribe(
-                async ([isLoggedIn, deviceInfo]) => {
-                    if (isLoggedIn && deviceInfo.isEnrolled) {
-                        this.deviceId = deviceInfo.deviceId
-                        
-                        const position = await Geolocation.getCurrentPosition()
-                            .catch(err => {
-                                console.log("Cannot get current GPS position.")
-                            })
-                        
+            .subscribe(async ([isLoggedIn, deviceInfo]) => {
+                if (isLoggedIn && deviceInfo.isEnrolled) {
+                    this.deviceId = deviceInfo.deviceId
+                    
+                    // Try to update current position
+                    const position = await Geolocation.getCurrentPosition()
+                        .catch(err => {
+                            console.log("Cannot get current GPS position.")
+                        })
+                    
+                    if (position?.coords) {
                         this.updateLocation(position)
-                        
-                        this.gpsWatchId = Geolocation.watchPosition(
-                            {"enableHighAccuracy": true},
-                            (
-                                position,
-                                err
-                            ) => {
-                                if (position != null) {
-                                    this.updateLocation(position)
-                                }
-                            })
+                    }
+                    
+                    // Don't setup listener if already listening
+                    if (this.gpsWatchId) {
+                        return
+                    }
+                    
+                    // Setup listener for location changes
+                    this.gpsWatchId = Geolocation.watchPosition(
+                        {"enableHighAccuracy": true},
+                        (
+                            position,
+                            err
+                        ) => {
+                            if (position?.coords) {
+                                this.updateLocation(position)
+                            }
+                        })
+                }
+                else {
+                    // Clear listener if not logged in or enrolled
+                    if (this.gpsWatchId) {
+                        Geolocation.clearWatch({id: this.gpsWatchId})
+                        this.gpsWatchId = null
                     }
                 }
-            )
+            })
     }
     
-    get location() {
-        return this._location$.getValue()
-    }
-    
-    get location$() {
+    get location$(): Observable<DeviceGpsLocationTuple | null> {
         return this._location$.asObservable()
     }
     
-    private get _location() {
-        return this.location
+    get location(): DeviceGpsLocationTuple | null {
+        return this._location$.getValue()
+    }
+    
+    private get _location(): DeviceGpsLocationTuple | null {
+        return this._location$.getValue()
     }
     
     private set _location(value) {
         this._location$.next(value)
     }
-
+    
     private updateLocation(position): void {
-        if (!position?.coords) {
-            return
-        }
-        const now = new Date() // in datetime with timezone
+        const now = new Date() // In datetime with timezone
         
-        // send to Peek Logic
+        // Send to Peek Logic
         const action = new GpsLocationUpdateTupleAction()
         action.latitude = position.coords.latitude
         action.longitude = position.coords.longitude
@@ -91,7 +102,7 @@ export class PrivateDeviceGpsLocationService extends DeviceGpsLocationService {
         this.lastSeenPositionTupleAction = action
         this.tupleService.tupleOfflineAction.pushAction(action)
         
-        // update location observable
+        // Update location observable
         const location = new DeviceGpsLocationTuple()
         location.latitude = position.coords.latitude
         location.longitude = position.coords.longitude
