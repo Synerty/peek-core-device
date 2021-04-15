f88737ec389c47df911d00b420e9c0d5c17c82ff
Jacob Sorrell <jacob.sorrell@synerty.com>
2021-04-15 21:40:41 +0000

    PEEK-1132 Fix Admin Device Updates
    - Add DeviceInfoTableTupleProvider
    
    - Add DeviceInfoTable
    - Update frontend to use DeviceInfoTable

--- peek_core_device/_private/admin-app/tuples/DeviceInfoTable.ts
+++ peek_core_device/_private/admin-app/tuples/DeviceInfoTable.ts
@@ -1,13 +1,12 @@
 import { addTupleType, Tuple } from "@synerty/vortexjs"
-import { deviceTuplePrefix } from "./_private/PluginNames"
-import { DeviceTypeEnum } from "./_private/hardware-info/hardware-info.abstract"
-import { DeviceGpsLocationTuple } from "./DeviceGpsLocationTuple"
+import { DeviceTypeEnum, deviceTuplePrefix } from "@peek/peek_core_device/_private"
+import { DeviceGpsLocationTuple } from "@peek/peek_core_device"
 import { Capacitor } from "@capacitor/core"
 
 @addTupleType
-export class DeviceInfoTuple extends Tuple {
-    public static readonly tupleName = deviceTuplePrefix + "DeviceInfoTuple"
-    
+export class DeviceInfoTable extends Tuple {
+    public static readonly tupleName = deviceTuplePrefix + "DeviceInfoTable"
+
     readonly TYPE_FIELD_IOS = "field-ios"
     readonly TYPE_FIELD_ANDROID = "field-android"
     readonly TYPE_MOBILE_WEB = "mobile-web"
@@ -19,6 +18,7 @@ export class DeviceInfoTuple extends Tuple {
     readonly DEVICE_ONLINE = 1
     readonly DEVICE_BACKGROUND = 2
 
+    id: string
     description: string
     deviceId: string
     deviceType: string
@@ -31,19 +31,19 @@ export class DeviceInfoTuple extends Tuple {
     deviceStatus: number
     isEnrolled: boolean
     currentLocation: DeviceGpsLocationTuple
-    
+
     constructor() {
-        super(DeviceInfoTuple.tupleName)
+        super(DeviceInfoTable.tupleName)
     }
-    
+
     get isWeb(): boolean {
         return !Capacitor.isNative
     }
-    
+
     get isBackgrounded(): boolean {
         return !!(this.deviceStatus & this.DEVICE_BACKGROUND)
     }
-    
+
     get googleMapLink() {
         if (!this.hasCurrentLocation()) {
             throw new Error("current location is not available")
@@ -51,36 +51,36 @@ export class DeviceInfoTuple extends Tuple {
         return "https://www.google.com/maps/search/?api=1&query="
             + `${this.currentLocation.latitude},${this.currentLocation.longitude}`
     }
-    
+
     setDeviceType(val: DeviceTypeEnum) {
         switch (val) {
             case DeviceTypeEnum.DESKTOP_WEB:
                 this.deviceType = this.TYPE_DESKTOP_WEB
                 break
-            
+
             case DeviceTypeEnum.DESKTOP_MACOS:
                 this.deviceType = this.TYPE_DESKTOP_MACOS
                 break
-            
+
             case DeviceTypeEnum.DESKTOP_WINDOWS:
                 this.deviceType = this.TYPE_DESKTOP_WINDOWS
                 break
-            
+
             case DeviceTypeEnum.FIELD_IOS:
                 this.deviceType = this.TYPE_FIELD_IOS
                 break
-            
+
             case DeviceTypeEnum.FIELD_ANDROID:
                 this.deviceType = this.TYPE_FIELD_ANDROID
                 break
-            
+
             case DeviceTypeEnum.MOBILE_WEB:
                 this.deviceType = this.TYPE_MOBILE_WEB
                 break
-            
+
         }
     }
-    
+
     hasCurrentLocation() {
         if (!this.currentLocation?.latitude) {
             return false
