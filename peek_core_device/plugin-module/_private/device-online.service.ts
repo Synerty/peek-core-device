a3ca2b1da1133546654c31e3950f75bb02b61508
Jarrod Chesney <jarrod.chesney@synerty.com>
2022-04-18 15:14:11 +1000

    PEEK-1488 Removed circular dependency
    for UpdateDeviceOnlineAction

--- peek_core_device/plugin-module/_private/device-online.service.ts
+++ peek_core_device/plugin-module/_private/device-online.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { DeviceTupleService } from "./device-tuple.service";
-import { UpdateDeviceOnlineAction } from "./";
+import { UpdateDeviceOnlineAction } from "./tuples/UpdateDeviceOnlineAction";
 
 @Injectable()
 export class DeviceOnlineService {
